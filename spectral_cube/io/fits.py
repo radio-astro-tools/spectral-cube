fe41458d47995646a5664645e58b0d54473fb8a1
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-14 19:16:50 -0400

    moved writer to io/fits.py

--- spectral_cube/io/fits.py
+++ spectral_cube/io/fits.py
@@ -3,6 +3,7 @@ from astropy.io import fits
 from astropy.wcs import WCS
 import numpy as np
 from spectral_cube import SpectralCube,SpectralCubeMask
+from . import wcs_manipulation
 
 
 def load_fits_cube(filename, extnum=0, **kwargs):
@@ -39,3 +40,26 @@ def load_fits_cube(filename, extnum=0, **kwargs):
     cube = SpectralCube(data, wcs, mask, meta=meta)
 
     return cube
+
+def write_fits_cube(filename, data, wcs, includestokes=False, clobber=False):
+    """
+    Write a FITS cube with a WCS to a filename
+
+    TODO: document further!!
+    """
+    axtypes = wcs.get_axis_types()
+    stokesax = np.array([a['coordinate_type'] == 'stokes' for a in axtypes])
+    if not includestokes:
+        if data.shape[0] != 1:
+            raise ValueError("Cannot drop stokes unless it's degenerate")
+        data = data[0,:,:,:]
+        if np.any(stokesax):
+            drop = np.argmax(stokesax)
+            wcs = wcs_manipulation.drop_axis(wcs, drop)
+    else:
+        if not np.any(stokesax):
+            wcs = wcs_manipulation.add_stokes_axis_to_wcs(wcs, 0)
+
+    header = wcs.to_header()
+    outhdu = fits.PrimaryHDU(data=data, header=header)
+    outhdu.writeto(filename, clobber=clobber)
