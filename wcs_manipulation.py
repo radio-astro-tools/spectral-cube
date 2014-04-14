5678d5939c555860ad809ae4cb3b5fe02ba6c1e7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-14 17:14:08 -0400

    WCS tests

--- wcs_manipulation.py
+++ wcs_manipulation.py
@@ -40,5 +40,52 @@ def reindex_wcs(wcs, inds):
     return outwcs
 
 
-def test_wcs_manipulation():
-    pass
+def test_wcs_dropping():
+    wcs = WCS(naxis=4)
+    wcs.wcs.pc = np.zeros([4,4])
+    np.fill_diagonal(wcs.wcs.pc, np.arange(1,5))
+    pc = wcs.wcs.pc # for later use below
+
+    dropped = drop_axis(wcs,0)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([2,3,4]))
+    dropped = drop_axis(wcs,1)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,3,4]))
+    dropped = drop_axis(wcs,2)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,2,4]))
+    dropped = drop_axis(wcs,3)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,2,3]))
+
+    wcs = WCS(naxis=4)
+    wcs.wcs.cd = pc
+
+    dropped = drop_axis(wcs,0)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([2,3,4]))
+    dropped = drop_axis(wcs,1)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,3,4]))
+    dropped = drop_axis(wcs,2)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,2,4]))
+    dropped = drop_axis(wcs,3)
+    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1,2,3]))
+
+def test_wcs_swapping():
+    wcs = WCS(naxis=4)
+    wcs.wcs.pc = np.zeros([4,4])
+    np.fill_diagonal(wcs.wcs.pc, np.arange(1,5))
+    pc = wcs.wcs.pc # for later use below
+
+    swapped = wcs_swapaxes(wcs,0,1)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([2,1,3,4]))
+    swapped = wcs_swapaxes(wcs,0,3)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([4,2,3,1]))
+    swapped = wcs_swapaxes(wcs,2,3)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([1,2,4,3]))
+
+    wcs = WCS(naxis=4)
+    wcs.wcs.cd = pc
+
+    swapped = wcs_swapaxes(wcs,0,1)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([2,1,3,4]))
+    swapped = wcs_swapaxes(wcs,0,3)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([4,2,3,1]))
+    swapped = wcs_swapaxes(wcs,2,3)
+    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([1,2,4,3]))
