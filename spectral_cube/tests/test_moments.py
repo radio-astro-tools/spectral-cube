f456dfe2b0a30ced49502f991c9145fe7f9ad346
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-16 11:03:07 -0400

    moments work?

--- spectral_cube/tests/test_moments.py
+++ spectral_cube/tests/test_moments.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from astropy.wcs import WCS
 from astropy.io import fits
@@ -15,11 +16,34 @@ def moment_cube():
     hdu = fits.PrimaryHDU(data=cube, header=wcs.to_header())
     return hdu
 
-def test_moment0():
+@pytest.mark.parametrize(('axis',),
+                         [(0,),
+                          (1,),
+                          (2,)])
+def test_moment0(axis):
     mc_hdu = moment_cube()
     sc = spfits.load_fits_hdu(mc_hdu)
 
-    mom0_0_np = mc_hdu.data.mean(axis=0)
-    mom0_0_sc = sc.moment(0, axis=0)
+    mom0_np = mc_hdu.data.mean(axis=axis)
+    mom0_sc = sc.moment(0, axis=axis)
     
-    np.testing.assert_array_almost_equal_nulp(mom0_0_np, mom0_0_sc.value)
+    np.testing.assert_array_almost_equal_nulp(mom0_np, mom0_sc.value)
+
+@pytest.mark.parametrize(('axis',),
+                         [(0,),
+                          (1,),
+                          (2,)])
+def test_moment1(axis):
+    mc_hdu = moment_cube()
+    sc = spfits.load_fits_hdu(mc_hdu)
+
+    pixcrds = np.array([x.ravel() for x in np.indices(sc.shape)]).T
+    world = sc._wcs.wcs_pix2world(pixcrds,0)
+    # I don't know why I have to transpose it, but the spectral coordinate
+    # NEEDS to go first, so this is how it is...
+    w = world[:,2-axis].reshape(sc.shape).T
+
+    mom1_np = (mc_hdu.data*w).sum(axis=axis) / (mc_hdu.data.sum(axis=axis))
+    mom1_sc = sc.moment(1, axis=axis)
+    
+    np.testing.assert_array_almost_equal_nulp(mom1_np, mom1_sc.value)
