82e87d2ca69c0cd2d7164f84900a4ebcbe61abd1
Chris Beaumont <beaumont@hawaii.edu>
2014-04-15 22:49:30 -0400

    New tests and some bugfixes

--- spectral_cube/tests/test_spectral_cube.py
+++ spectral_cube/tests/test_spectral_cube.py
@@ -1,16 +1,12 @@
-import os
-
 import pytest
 from astropy.io import fits
+from astropy import units as u
 from astropy.wcs import WCS
 
 import numpy as np
 
 from spectral_cube import SpectralCube, SpectralCubeMask, read
-
-
-def path(filename):
-    return os.path.join(os.path.dirname(__file__), 'data', filename)
+from . import path
 
 
 def cube_and_raw(filename):
@@ -39,22 +35,10 @@ class TestSpectralCube(object):
         expected = np.squeeze(d.transpose(trans))
         np.testing.assert_array_equal(c.get_data(), expected)
 
-    def test_mask_data(self):
-        c, d = cube_and_raw('advs.fits')
-        d = d[0]
-        wcs = c._wcs
-        mask = SpectralCubeMask(d > .1, wcs)
-        c._mask = mask
-
-        expected = np.where(d > .1, d, np.nan)
-        np.testing.assert_array_equal(c.get_data(), expected)
-
-        expected = np.where(d > .1, d, 0)
-        np.testing.assert_array_equal(c.get_data(fill=0), expected)
-
     @pytest.mark.parametrize(('file', 'view'), (
                              ('adv.fits', np.s_[:, :, :]),
                              ('adv.fits', np.s_[::2, :, :2]),
+                             ('adv.fits', np.s_[0]),
                              ))
     def test_world(self, file, view):
         p = path(file)
@@ -68,7 +52,8 @@ class TestSpectralCube(object):
         world = wcs.all_pix2world(pix, 0).T
 
         world = [w.reshape(shp) for w in world]
-        world = [w[view] for w in world][::-1]
+        world = [w[view] * u.Unit(wcs.wcs.cunit[i])
+                 for i, w in enumerate(world)][::-1]
 
         w2 = c.world[view]
         for result, expected in zip(w2, world):
@@ -76,16 +61,200 @@ class TestSpectralCube(object):
 
     @pytest.mark.parametrize('view', (np.s_[:, :, :],
                              np.s_[:2, :3, ::2]))
-    def test_world_transposes(self, view):
+    def test_world_transposes_3d(self, view):
         c1, d1 = cube_and_raw('adv.fits')
         c2, d2 = cube_and_raw('vad.fits')
 
         for w1, w2 in zip(c1.world[view], c2.world[view]):
             np.testing.assert_array_equal(w1, w2)
 
-"""
-TODO:
+    @pytest.mark.parametrize('view',
+                             (np.s_[:, :, :],
+                              np.s_[:2, :3, ::2],
+                              np.s_[::3, ::2, :1],
+                              np.s_[:], ))
+    def test_world_transposes_4d(self, view):
+        c1, d1 = cube_and_raw('advs.fits')
+        c2, d2 = cube_and_raw('sadv.fits')
+        for w1, w2 in zip(c1.world[view], c2.world[view]):
+            np.testing.assert_array_equal(w1, w2)
 
- check that pix<->world is correct for all transpositions
 
- """
+class TestFilters(object):
+
+    def setup_method(self, method):
+        c, d = cube_and_raw('advs.fits')
+        d = d[0]
+        wcs = c._wcs
+        mask = SpectralCubeMask(d > .1, wcs)
+        self.d = d
+        self.c = c
+        self.mask = mask
+        c._mask = mask
+
+    def test_mask_data(self):
+        c, d = self.c, self.d
+
+        expected = np.where(d > .1, d, np.nan)
+        np.testing.assert_array_equal(c.get_data(), expected)
+
+        expected = np.where(d > .1, d, 0)
+        np.testing.assert_array_equal(c.get_data(fill=0), expected)
+
+    def test_flatten(self):
+        c, d = self.c, self.d
+        expected = d[d > 0.1]
+        np.testing.assert_array_equal(c.flattened(), expected)
+
+    def test_flatten_weights(self):
+        c, d = self.c, self.d
+        expected = d[d > 0.1] ** 2
+        np.testing.assert_array_equal(c.flattened(weights=d), expected)
+
+    @pytest.mark.xfail
+    def test_slice(self):
+        c, d = self.c, self.d
+
+        expected = d[:3, :2, ::2]
+        expected = expected[expected > 0.1]
+        np.testing.assert_array_equal(c[0:3, 0:2, 0::2].flattened(), expected)
+
+
+class TestNumpyMethods(object):
+
+    def setup_method(self, method):
+        c, d = cube_and_raw('adv.fits')
+        mask = SpectralCubeMask(d > 0.5, c._wcs)
+        c._mask = mask
+        self.c = c
+        self.mask = mask
+        self.d = d
+
+    def _check_numpy(self, cubemethod, array, func):
+        for axis in [None, 0, 1, 2]:
+            expected = func(array, axis=axis)
+            actual = cubemethod(axis=axis)
+            np.testing.assert_array_equal(actual, expected)
+
+    def test_sum(self):
+        d = np.where(self.d > 0.5, self.d, np.nan)
+        self._check_numpy(self.c.sum, d, np.nansum)
+
+    def test_max(self):
+        d = np.where(self.d > 0.5, self.d, np.nan)
+        self._check_numpy(self.c.max, d, np.nanmax)
+
+    def test_min(self):
+        d = np.where(self.d > 0.5, self.d, np.nan)
+        self._check_numpy(self.c.min, d, np.nanmin)
+
+    def test_argmax(self):
+        d = np.where(self.d > 0.5, self.d, -10)
+        self._check_numpy(self.c.argmax, d, np.nanargmax)
+
+    def test_argmin(self):
+        d = np.where(self.d > 0.5, self.d, 10)
+        self._check_numpy(self.c.argmin, d, np.nanargmin)
+
+    def test_median(self):
+        m = np.empty(self.d.sum(axis=0).shape)
+        for y in range(m.shape[0]):
+            for x in range(m.shape[1]):
+                ray = self.d[:, y, x]
+                ray = ray[ray > 0.5]
+                m[y, x] = np.median(ray)
+        np.testing.assert_array_equal(self.c.median(axis=0), m)
+
+    def test_percentile(self):
+        m = np.empty(self.d.sum(axis=0).shape)
+        for y in range(m.shape[0]):
+            for x in range(m.shape[1]):
+                ray = self.d[:, y, x]
+                ray = ray[ray > 0.5]
+                m[y, x] = np.percentile(ray, 3)
+        np.testing.assert_array_equal(self.c.percentile(3, axis=0), m)
+
+    @pytest.mark.parametrize('method', ('sum', 'min', 'max',
+                             'median', 'argmin', 'argmax'))
+    def test_transpose(self, method):
+        c1, d1 = cube_and_raw('adv.fits')
+        c2, d2 = cube_and_raw('vad.fits')
+        for axis in [None, 0, 1, 2]:
+            np.testing.assert_array_equal(getattr(c1, method)(axis=axis),
+                                          getattr(c2, method)(axis=axis))
+
+
+class TestMoment(object):
+
+    def setup_method(self, method):
+        c, d = cube_and_raw('adv.fits')
+        mask = SpectralCubeMask(d > 0.5, c._wcs)
+        c._mask = mask
+        self.c = c
+        self.mask = mask
+        self.d = d
+
+    @pytest.mark.xfail
+    def test_mom0(self):
+        c = self.c
+        np.testing.assert_array_equal(c.moment(0, axis=0), c.sum(axis=0))
+
+    @pytest.mark.parametrize('axis', (0,))
+    def test_mom1(self, axis):
+        c = self.c
+        d = np.where(self.d > 0.5, self.d, np.nan)
+        w = self.c.world[:][axis].value
+
+        result = c.moment(1, axis=axis).value
+        expected = np.nansum(w * d, axis=axis) / np.nansum(d, axis=axis)
+        np.testing.assert_array_equal(result, expected)
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('axis', (1, 2))
+    def test_mom1_ax12(self, axis):
+        c = self.c
+        d = np.where(self.d > 0.5, self.d, np.nan)
+        w = self.c.world[:][axis].value
+
+        result = c.moment(1, axis=axis).value
+        expected = np.nansum(w * d, axis=axis) / np.nansum(d, axis=axis)
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestSlab(object):
+
+    def setup_method(self, method):
+        c, d = cube_and_raw('adv.fits')
+        mask = SpectralCubeMask(d > 0.5, c._wcs)
+        c._mask = mask
+        self.c = c
+        self.mask = mask
+        self.d = d
+
+    def test_closest_spectral_channel(self):
+        c = self.c
+        ms = u.m / u.s
+        assert c.closest_spectral_channel(-321214.698632 * ms) == 0
+        assert c.closest_spectral_channel(-319926.48366321 * ms) == 1
+        assert c.closest_spectral_channel(-318638.26869442 * ms) == 2
+
+        assert c.closest_spectral_channel(-320000 * ms) == 1
+        assert c.closest_spectral_channel(-340000 * ms) == 0
+        assert c.closest_spectral_channel(0 * ms) == 3
+
+    def test_spectral_channel_bad_units(self):
+        with pytest.raises(u.UnitsError):
+            self.c.closest_spectral_channel(0 * u.s, rest_frequency=1 / u.s)
+
+        with pytest.raises(u.UnitsError):
+            self.c.closest_spectral_channel(0 * u.s)
+
+    def test_slab(self):
+        ms = u.m / u.s
+        c2 = self.c.spectral_slab(-320000 * ms, -318600 * ms)
+        np.testing.assert_array_equal(c2._data, self.d[1:3])
+
+    def test_slab_reverse_limits(self):
+        ms = u.m / u.s
+        c2 = self.c.spectral_slab(-318600 * ms, -320000 * ms)
+        np.testing.assert_array_equal(c2._data, self.d[1:3])
--- spectral_cube/wcs_utils.py
+++ spectral_cube/wcs_utils.py
@@ -144,70 +144,10 @@ def axis_names(wcs):
     -------
     A tuple of names along each axis
     """
-    names = wcs.wcs.cname
+    names = list(wcs.wcs.cname)
     types = wcs.wcs.ctype
     for i in range(len(names)):
         if len(names[i]) > 0:
             continue
-        names[i] = types.split('-')[0]
+        names[i] = types[i].split('-')[0]
     return names
-
-
-def test_wcs_dropping():
-    wcs = WCS(naxis=4)
-    wcs.wcs.pc = np.zeros([4, 4])
-    np.fill_diagonal(wcs.wcs.pc, np.arange(1, 5))
-    pc = wcs.wcs.pc  # for later use below
-
-    dropped = drop_axis(wcs, 0)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([2, 3, 4]))
-    dropped = drop_axis(wcs, 1)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 3, 4]))
-    dropped = drop_axis(wcs, 2)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 2, 4]))
-    dropped = drop_axis(wcs, 3)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 2, 3]))
-
-    wcs = WCS(naxis=4)
-    wcs.wcs.cd = pc
-
-    dropped = drop_axis(wcs, 0)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([2, 3, 4]))
-    dropped = drop_axis(wcs, 1)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 3, 4]))
-    dropped = drop_axis(wcs, 2)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 2, 4]))
-    dropped = drop_axis(wcs, 3)
-    assert np.all(dropped.wcs.get_pc().diagonal() == np.array([1, 2, 3]))
-
-
-def test_wcs_swapping():
-    wcs = WCS(naxis=4)
-    wcs.wcs.pc = np.zeros([4, 4])
-    np.fill_diagonal(wcs.wcs.pc, np.arange(1, 5))
-    pc = wcs.wcs.pc  # for later use below
-
-    swapped = wcs_swapaxes(wcs, 0, 1)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([2, 1, 3, 4]))
-    swapped = wcs_swapaxes(wcs, 0, 3)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([4, 2, 3, 1]))
-    swapped = wcs_swapaxes(wcs, 2, 3)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([1, 2, 4, 3]))
-
-    wcs = WCS(naxis=4)
-    wcs.wcs.cd = pc
-
-    swapped = wcs_swapaxes(wcs, 0, 1)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([2, 1, 3, 4]))
-    swapped = wcs_swapaxes(wcs, 0, 3)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([4, 2, 3, 1]))
-    swapped = wcs_swapaxes(wcs, 2, 3)
-    assert np.all(swapped.wcs.get_pc().diagonal() == np.array([1, 2, 4, 3]))
-
-
-def test_add_stokes():
-    wcs = WCS(naxis=3)
-
-    for ii in range(4):
-        outwcs = add_stokes_axis_to_wcs(wcs, ii)
-        assert outwcs.wcs.naxis == 4
