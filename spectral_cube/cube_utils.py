a3d516aec5a9a6c9d62ddb42d99b0d91d686cb9e
Chris Beaumont <beaumont@hawaii.edu>
2014-04-29 18:11:05 -0400

    doc typos

--- spectral_cube/cube_utils.py
+++ spectral_cube/cube_utils.py
@@ -144,7 +144,7 @@ class SliceIndexer(object):
 def iterator_strategy(cube, axis=None):
     """
     Guess the most efficient iteration strategy
-    for iterating over a cube, given it's size and layout
+    for iterating over a cube, given its size and layout
 
     Parameters
     ----------
@@ -159,7 +159,7 @@ def iterator_strategy(cube, axis=None):
     strategy : ['cube' | 'ray' | 'slice']
         The recommended iteration strategy.
         *cube* recommends working with the entire array in memory
-        *slice* recomends working with one slice at a time
+        *slice* recommends working with one slice at a time
         *ray*  recommends working with one ray at a time
     """
     # pretty simple for now
