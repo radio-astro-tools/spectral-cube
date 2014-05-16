a98e5868135c3b1b393f1fa89ba4d4b1ab5cc693
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-05-16 14:49:52 +0200

    make WCS comparison more permissive; WCS are most likely to be identical if one has some info (e.g. equinox) and the other does not, so it is better to warn than Raise for this case.

--- spectral_cube/wcs_utils.py
+++ spectral_cube/wcs_utils.py
@@ -215,6 +215,13 @@ def check_equality(wcs1, wcs2, warn_missing=False, verbose=False):
                     OK = False
                     if verbose:
                         print("Header 1, {0}: {1} != {2}".format(key,u1,u2))
+            elif isinstance(c1[1], (float, np.float)):
+                try:
+                    np.testing.assert_almost_equal(c1[1], c2[1])
+                except AssertionError:
+                    if verbose:
+                        print("Header 1, {0}: {1} != {2}".format(key,c1[1],c2[1]))
+                    OK = False
             elif c1[1] != c2[1]:
                 if verbose:
                     print("Header 1, {0}: {1} != {2}".format(key,c1[1],c2[1]))
