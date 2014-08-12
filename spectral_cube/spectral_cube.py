41bd7d60870e3c6f492ac7ab02566321d0d443e4
John ZuHone <jzuhone@gmail.com>
2014-08-12 04:11:04 -0400

    Followed @astrofrog's suggestion and made these methods take the spectral_cube argument

--- spectral_cube/spectral_cube.py
+++ spectral_cube/spectral_cube.py
@@ -1279,8 +1279,6 @@ class SpectralCube(object):
         import yt
         yt_version = float(yt.__version__.split("-")[0])
 
-        self.yt_spectral_factor = spectral_factor
-
         if yt_version >= 3.0:
 
             from yt.frontends.fits.api import FITSDataset
