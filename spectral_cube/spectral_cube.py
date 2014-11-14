6bae380aebca887c2ac827cd2ba909d08ff0fceb
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-11-14 16:07:27 +0100

    add a quicklook tool for spectra

--- spectral_cube/spectral_cube.py
+++ spectral_cube/spectral_cube.py
@@ -191,6 +191,18 @@ class OneDSpectrum(LowerDimensionalObject):
 
         return self
 
+    def quicklook(self, **kwargs):
+        """
+        Simply plot the spectrum
+
+        kwargs are passed to `matplotlib.pyplot.plot`
+        """
+        from matplotlib import pyplot
+        xaxis, = self.wcs.wcs_pix2world([np.arange(self.size)],0)
+        self.figure = pyplot.plot(xaxis, self.value, **kwargs)
+        pyplot.gca().set_xlabel(self.wcs.wcs.cunit[0])
+        pyplot.gca().set_ylabel(self.unit)
+
 
 class SpectralCube(object):
 
