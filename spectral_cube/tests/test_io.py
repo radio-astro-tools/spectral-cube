9a011dbb11009c8d67e7d9861aef2459fad231c3
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-11 14:15:44 +0200

    add a test for the new 3d/4d checker

--- spectral_cube/tests/test_io.py
+++ spectral_cube/tests/test_io.py
@@ -1,3 +1,4 @@
+from astropy.io import fits as pyfits
 from ..io import class_lmv, fits
 from .. import SpectralCube
 from . import path
@@ -14,3 +15,15 @@ def test_lmv_fits():
 
     # should also be able to do this, but it is again false:
     #assert c1.wcs==c2.wcs
+
+def test_3d_4d_stokes():
+    f3 = pyfits.open(path('adv.fits'))
+    f4 = pyfits.open(path('advs.fits'))
+    f3b = pyfits.PrimaryHDU(data=f3[0].data, header=f4[0].header)
+
+    c1 = SpectralCube.read(f3)
+    c2 = SpectralCube.read(f4)
+    c3 = SpectralCube.read(f3b)
+
+    assert c1.shape == c3.shape
+    # c2 has a different shape on disk...
