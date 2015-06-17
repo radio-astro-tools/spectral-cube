5ffb3e20fd60b82f0156da28104b75e8357f3c9c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-17 11:47:29 +0200

    remove subcube tests from test_spectral_cube

--- spectral_cube/tests/test_spectral_cube.py
+++ spectral_cube/tests/test_spectral_cube.py
@@ -472,35 +472,6 @@ def test_preserve_spectral_unit():
     new_cube = cube_freq.with_fill_value(fill_value=3.4)
     assert new_cube.spectral_axis.unit is u.GHz
 
-def test_subcube():
-
-    cube, data = cube_and_raw('advs.fits')
-
-    sc1 = cube.subcube(xlo=1, xhi=3)
-    sc2 = cube.subcube(xlo=24.06269*u.deg, xhi=24.06206*u.deg)
-
-    assert sc1.shape == (2,3,2)
-    assert sc2.shape == (2,3,2)
-    assert sc1.wcs.wcs.compare(sc2.wcs.wcs)
-
-    sc3 = cube.subcube()
-
-    assert sc3.shape == cube.shape
-    assert sc3.wcs.wcs.compare(cube.wcs.wcs)
-    assert np.all(sc3._data == cube._data)
-
-def test_ds9region():
-    pass
-
-
-    #region = 'fk5\ncircle(29.9346557, 24.0623827, 0.11111)'
-    #subcube = cube.subcube_from_ds9region(region)
-    # THIS TEST FAILS!
-    # I think the coordinate transformation in ds9 is wrong;
-    # it uses kapteyn?
-    
-    #region = 'circle(2,2,2)'
-    #subcube = cube.subcube_from_ds9region(region)
 
 @pytest.mark.skipif(not bottleneckOK, reason='Bottleneck could not be imported')
 def test_endians():
