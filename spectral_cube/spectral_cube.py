ebfa3ad7608be6b7333276d218df3bc0d9000644
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-07-06 21:38:36 +0200

    some functions can return unitless things

--- spectral_cube/spectral_cube.py
+++ spectral_cube/spectral_cube.py
@@ -584,11 +584,12 @@ class SpectralCube(object):
             # acquire the flattened, valid data for the slice
             data = self.flattened(slc, weights=weights)
             if len(data) != 0:
-                if hasattr(data, 'value'):
+                result = function(data, **kwargs)
+                if hasattr(result, 'value'):
                     # store result in array
-                    out[x, y] = function(data, **kwargs).value
+                    out[x, y] = result.value
                 else:
-                    out[x, y] = function(data, **kwargs)
+                    out[x, y] = result
 
         if projection and axis in (0,1,2):
             new_wcs = wcs_utils.drop_axis(self._wcs, np2wcs[axis])
