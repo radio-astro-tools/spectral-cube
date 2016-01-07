e125c65bf8915b83e010bbe65dac85c10703841e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-01-07 17:55:52 +0100

    add "mirror" tests for OneDSpectrum and Slice where possible & appropriate

--- spectral_cube/tests/test_projection.py
+++ spectral_cube/tests/test_projection.py
@@ -1,35 +1,57 @@
 from __future__ import print_function, absolute_import, division
 
+import pytest
 import numpy as np
 from astropy import units as u
 
 from .helpers import assert_allclose
-from ..lower_dimensional_structures import Projection
+from ..lower_dimensional_structures import Projection, Slice, OneDSpectrum
 
-def test_slices_of_projections_not_projections():
+# set up for parametrization
+LDOs = (Projection, Slice, OneDSpectrum)
+LDOs_2d = (Projection, Slice,)
+
+two_qty_2d = np.ones((2,2)) * u.Jy
+twelve_qty_2d = np.ones((12,12)) * u.Jy
+two_qty_1d = np.ones((2,)) * u.Jy
+twelve_qty_1d = np.ones((12,)) * u.Jy
+
+data_two = (two_qty_2d, two_qty_2d, two_qty_1d)
+data_twelve = (twelve_qty_2d, twelve_qty_2d, twelve_qty_1d)
+data_two_2d = (two_qty_2d, two_qty_2d,)
+data_twelve_2d = (twelve_qty_2d, twelve_qty_2d,)
+
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs_2d, data_two_2d))
+def test_slices_of_projections_not_projections(LDO, data):
     # slices of projections that have <2 dimensions should not be projections
-    image = np.ones((2, 2)) * u.Jy
-    p = Projection(image, copy=False)
+    p = LDO(data, copy=False)
 
-    assert not isinstance(p[0,0], Projection)
-    assert not isinstance(p[0], Projection)
+    assert not isinstance(p[0,0], LDO)
+    assert not isinstance(p[0], LDO)
 
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs_2d, data_twelve_2d))
+def test_copy_false(LDO, data):
+    # copy the data so we can manipulate inplace without affecting other tests
+    image = data.copy()
 
-def test_copy_false():
-    image = np.ones((12, 12)) * u.Jy
-    p = Projection(image, copy=False)
+    p = LDO(image, copy=False)
     image[3,4] = 2 * u.Jy
     assert_allclose(p[3,4], 2 * u.Jy)
 
-def test_write(tmpdir):
-    image = np.ones((12, 12)) * u.Jy
-    p = Projection(image)
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs, data_twelve))
+def test_write(LDO, data, tmpdir):
+    p = LDO(data)
     p.write(tmpdir.join('test.fits').strpath)
 
-def test_preserve_wcs_to():
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs_2d, data_twelve_2d))
+def test_preserve_wcs_to(LDO, data):
     # regression for #256
-    image = np.ones((12, 12)) * u.Jy
-    p = Projection(image, copy=False)
+    image = data.copy()
+    p = LDO(image, copy=False)
     image[3,4] = 2 * u.Jy
 
     p2 = p.to(u.mJy)
@@ -38,11 +60,12 @@ def test_preserve_wcs_to():
 
     assert p2.wcs == p.wcs
 
-def test_multiplication():
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs, data_twelve))
+def test_multiplication(LDO, data):
     # regression: 265
 
-    image = np.ones((12, 12)) * u.Jy
-    p = Projection(image, copy=False)
+    p = LDO(data, copy=False)
 
     p2 = p * 5
 
@@ -51,11 +74,13 @@ def test_multiplication():
     assert p2.wcs == p.wcs
     assert np.all(p2.value == 5)
 
-def test_unit_division():
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs, data_twelve))
+def test_unit_division(LDO, data):
     # regression: 265
 
-    image = np.ones((12, 12)) * u.Jy
-    p = Projection(image, copy=False)
+    image = data
+    p = LDO(image, copy=False)
 
     p2 = p / u.beam
 
@@ -63,14 +88,35 @@ def test_unit_division():
     assert hasattr(p2, '_wcs')
     assert p2.wcs == p.wcs
 
-def test_isnan():
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs_2d, data_twelve_2d))
+def test_isnan(LDO, data):
     # Check that np.isnan strips units
 
-    image = np.ones((12, 12)) * u.Jy
+    image = data.copy()
     image[5,6] = np.nan
-    p = Projection(image, copy=False)
+    p = LDO(image, copy=False)
 
     mask = np.isnan(p)
 
     assert mask.sum() == 1
     assert not hasattr(mask, 'unit')
+
+@pytest.mark.parametrize(('LDO', 'data'),
+                         zip(LDOs, data_twelve))
+def test_self_arith(LDO, data):
+
+    image = data
+    p = LDO(image, copy=False)
+
+    p2 = p + p
+
+    assert hasattr(p2, '_wcs')
+    assert p2.wcs == p.wcs
+    assert np.all(p2.value==2)
+
+    p2 = p - p
+
+    assert hasattr(p2, '_wcs')
+    assert p2.wcs == p.wcs
+    assert np.all(p2.value==0)
