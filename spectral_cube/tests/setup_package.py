4d074443007507e106f21f99808a2749a5496ce7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-05-06 16:21:13 -0400

    re-enable the ds9 region tests and make sure they actually work

--- spectral_cube/tests/setup_package.py
+++ spectral_cube/tests/setup_package.py
@@ -2,5 +2,5 @@ from __future__ import print_function, absolute_import, division
 
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.fits', 'data/*.hdr', 'data/*.lmv']
+        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.fits', 'data/*.hdr', 'data/*.lmv', 'data/*reg']
     }
