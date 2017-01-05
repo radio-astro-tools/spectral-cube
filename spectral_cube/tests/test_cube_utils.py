0749b0f132b381c44c4af0081c1265ab3a308e8d
e-koch <koch.eric.w@gmail.com>
2017-01-05 14:21:54 -0800

    Clean-up imports

--- spectral_cube/tests/test_cube_utils.py
+++ spectral_cube/tests/test_cube_utils.py
@@ -1,9 +1,4 @@
 import pytest
-import numpy as np
-from astropy import units as u
-from astropy import convolution
-from astropy.wcs import WCS
-from astropy import wcs
 
 from .test_spectral_cube import cube_and_raw
 from ..cube_utils import largest_beam, smallest_beam
@@ -13,13 +8,6 @@ try:
     RADIO_BEAM_INSTALLED = True
 except ImportError:
     RADIO_BEAM_INSTALLED = False
-
-try:
-    import reproject
-    REPROJECT_INSTALLED = True
-except ImportError:
-    REPROJECT_INSTALLED = False
-
 
 @pytest.mark.skipif('not RADIO_BEAM_INSTALLED')
 def test_largest_beam():
