ec1d88dfab4816acae6cc24e65cb84fd19431426
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-28 19:27:21 -0600

    try installing hashlib inside CASA instead of via conda, which doesn't recognize it...

--- .install_pip.py
+++ .install_pip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from setuptools.command import easy_install
+easy_install.main(['--user', 'hashlib'])
 easy_install.main(['--user', 'pip'])
 
 print("pip installed.")
