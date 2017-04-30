a27073600c5fcc33ad9270a3e0eb75edddf4fe65
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-30 10:53:52 -0600

    urllib3 is also needed....

--- .install_pip.py
+++ .install_pip.py
@@ -2,6 +2,7 @@
 import sys
 from setuptools.command import easy_install
 
+easy_install.main(['--user', 'urllib3-1.21.tar.gz'])
 easy_install.main(['--user', 'pip-9.0.1.tar.gz'])
 # fails because CASA wasn't compiled against good versions of hash/sha lib
 #easy_install.main(['--user', 'pip'])
