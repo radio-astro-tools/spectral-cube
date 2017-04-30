42147a8649ece58863c078580aabbaa6320372c8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-30 10:37:40 -0600

    travis can't run *any* hash checking, so pip always fails.  Download directly instead

--- .install_astropy.py
+++ .install_astropy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import pip
 import sys
-pip.main(['install', 'numpy', '--user'])
-pip.main(['install', 'astropy', '--user'])
+pip.main(['install', '--user', 'numpy-1.12.1.zip'])
+pip.main(['install', '--user', 'astropy-1.3.2.tar.gz'])
+#pip.main(['install', 'numpy', '--user'])
+#pip.main(['install', 'astropy', '--user'])
 
 print("astropy installed")
 
