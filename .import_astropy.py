a0be61df678b890579e61055a07618c173c73b86
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-01 11:52:48 -0600

    print out more information during testing, and this time actually make the python setup.py part not happen when CASA is running

--- .import_astropy.py
+++ .import_astropy.py
@@ -7,5 +7,7 @@ import numpy as np
 
 print("astropy imported successfully.  "
       "Version: {0}, path: {1}".format(astropy.__version__, astropy.__path__))
+print("numpy imported successfully.  "
+      "Version: {0}, path: {1}".format(numpy.__version__, numpy.__path__))
 
 sys.exit(0)
