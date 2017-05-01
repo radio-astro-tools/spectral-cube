e30fa1aa6239bdf5bb6e7d96efcf0516122c9522
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-01 11:55:49 -0600

    oops, bad numpy import in last commit.

--- .import_astropy.py
+++ .import_astropy.py
@@ -5,9 +5,10 @@ import sys
 from astropy.io import fits
 import numpy as np
 
+print('sys.path: {0}'.format(sys.path))
 print("astropy imported successfully.  "
       "Version: {0}, path: {1}".format(astropy.__version__, astropy.__path__))
 print("numpy imported successfully.  "
-      "Version: {0}, path: {1}".format(numpy.__version__, numpy.__path__))
+      "Version: {0}, path: {1}".format(np.__version__, np.__path__))
 
 sys.exit(0)
