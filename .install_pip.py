0d770110f86a887d2886ef3e59919f1cdede35cd
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-30 22:14:25 -0600

    what if we don't install hashlib?  Will it use the CASA version instead?

--- .install_pip.py
+++ .install_pip.py
@@ -2,7 +2,7 @@
 import sys
 from setuptools.command import easy_install
 
-easy_install.main(['--user', 'hashlib-20081119.zip'])
+#easy_install.main(['--user', 'hashlib-20081119.zip'])
 easy_install.main(['--user', 'urllib3-1.21.tar.gz'])
 easy_install.main(['--user', 'pip-9.0.1.tar.gz'])
 # fails because CASA wasn't compiled against good versions of hash/sha lib
