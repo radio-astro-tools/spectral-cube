cf6a68c11b04dcd9536c2cec53591c9a9a4ffca4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-02 13:48:51 -0600

    remove some old debug stuff and don't try to install numpy

--- .install_astropy.py
+++ .install_astropy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pip
 import sys
-pip.main(['install', '--user', 'numpy-1.12.1.zip'])
+#pip.main(['install', '--user', 'numpy-1.12.1.zip'])
 pip.main(['install', '--user', 'astropy-1.3.2.tar.gz'])
 #pip.main(['install', 'numpy', '--user'])
 #pip.main(['install', 'astropy', '--user'])
