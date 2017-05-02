4dd7d640169212824d9f31efa0913bd2d8476f24
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-02 13:40:00 -0600

    it looks like we're importing the right numpy, but not when we install spectral-cube

--- .casa_setup.py
+++ .casa_setup.py
@@ -6,4 +6,9 @@ paths = os.getenv('PYPATH').split(":")
 for pp in paths:
     sys.path.insert(0, pp)
 
+import numpy as np
+print("numpy imported successfully.  "
+      "Version: {0}, path: {1}".format(np.__version__, np.__path__))
+print("Numpy has nanmedian: {0}".format(hasattr(np, 'nanmedian')))
+
 exec(open("./setup.py").read())
