01334cbee638ff36b0591bde88522e6cfd11a1b9
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-02 13:11:23 -0600

    check for nanmedian

--- .import_astropy.py
+++ .import_astropy.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-import astropy
 import sys
+import os
+
+paths = os.getenv('PYPATH').split(":")
+
+for pp in paths:
+    sys.path.insert(0, pp)
+
+import astropy
 
 from astropy.io import fits
 import numpy as np
@@ -10,5 +17,6 @@ print("astropy imported successfully.  "
       "Version: {0}, path: {1}".format(astropy.__version__, astropy.__path__))
 print("numpy imported successfully.  "
       "Version: {0}, path: {1}".format(np.__version__, np.__path__))
+print("Numpy has nanmedian: {0}".format(hasattr(np, 'nanmedian')))
 
 sys.exit(0)
