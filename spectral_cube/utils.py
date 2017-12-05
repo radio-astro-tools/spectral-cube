f36be7c569ca21937c5bb8e3381228156d3edee2
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-12-04 21:06:13 -0700

    have warnings apply to things with `how='cube'`

--- spectral_cube/utils.py
+++ spectral_cube/utils.py
@@ -24,14 +24,15 @@ def warn_slow(function):
 
     @wraps(function)
     def wrapper(self, *args, **kwargs):
-        if self._is_huge and not self.allow_huge_operations:
+        warn_how = ('how' in kwargs and kwargs['how'] == 'cube') or 'how' not in kwargs
+        if self._is_huge and not self.allow_huge_operations and warn_how:
             raise ValueError("This function ({0}) requires loading the entire "
                              "cube into memory, and the cube is large ({1} "
                              "pixels), so by default we disable this operation. "
                              "To enable the operation, set "
                              "`cube.allow_huge_operations=True` and try again."
                              .format(str(function), self.size))
-        elif not self._is_huge:
+        elif warn_how and not self._is_huge:
             # TODO: add check for whether cube has been loaded into memory
             warnings.warn("This function ({0}) requires loading the entire cube into "
                           "memory and may therefore be slow.".format(str(function)))
