685162a5844e9980f6ad6a305b694994b94e9f3a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-03-21 14:17:41 -0600

    add some basic performance tests that are skipped by default

--- spectral_cube/tests/test_performance.py
+++ spectral_cube/tests/test_performance.py
@@ -4,9 +4,14 @@ Performance-related tests to make sure we don't use more memory than we should
 
 from __future__ import print_function, absolute_import, division
 
+import pytest
+
 from .test_moments import moment_cube
 from .helpers import assert_allclose
 from ..spectral_cube import SpectralCube
+from . import utilities
+
+from astropy import convolution
 
 def find_base_nbytes(obj):
     # from http://stackoverflow.com/questions/34637875/size-of-numpy-strided-array-broadcast-array-in-memory
@@ -50,3 +55,33 @@ def test_pix_cen():
     assert find_base_nbytes(s) == sc.shape[0]*bytes_per_pix
     assert find_base_nbytes(y) == sc.shape[1]*sc.shape[2]*bytes_per_pix
     assert find_base_nbytes(x) == sc.shape[1]*sc.shape[2]*bytes_per_pix
+
+@pytest.mark.skipif('True')
+def test_parallel_performance_smoothing():
+
+    import timeit
+
+    setup = 'cube,_ = utilities.generate_gaussian_cube(shape=(300,100,100))'
+    stmt = 'result = cube.spectral_smooth(kernel=convolution.Gaussian1DKernel(1.0), num_cores={0}, use_memmap=False)'
+
+    rslt = {}
+    for ncores in (1,2,3,4):
+        time = timeit.timeit(stmt=stmt.format(ncores), setup=setup, number=5, globals=globals())
+        rslt[ncores] = time
+
+    print()
+    print("memmap=False")
+    print(rslt)
+
+    setup = 'cube,_ = utilities.generate_gaussian_cube(shape=(300,100,100))'
+    stmt = 'result = cube.spectral_smooth(kernel=convolution.Gaussian1DKernel(1.0), num_cores={0}, use_memmap=True)'
+
+    rslt = {}
+    for ncores in (1,2,3,4):
+        time = timeit.timeit(stmt=stmt.format(ncores), setup=setup, number=5, globals=globals())
+        rslt[ncores] = time
+
+    print()
+    print("memmap=True")
+    print(rslt)
+
