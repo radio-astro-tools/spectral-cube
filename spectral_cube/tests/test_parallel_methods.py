1368003821028fec55366d5466f434eb28ecd6fe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-08-03 16:41:12 -0600

    skip tests when dask not installed

--- spectral_cube/tests/test_parallel_methods.py
+++ spectral_cube/tests/test_parallel_methods.py
@@ -1,5 +1,5 @@
 """
-Performance-related tests to make sure we don't use more memory than we should
+Parallel function tests for correctness (surprisingly difficult to achieve)
 """
 
 from __future__ import print_function, absolute_import, division
@@ -7,33 +7,26 @@ from __future__ import print_function, absolute_import, division
 import numpy as np
 
 import pytest
-import tempfile
-import sys
 
-try:
-    import tracemalloc
-    tracemallocOK = True
-except ImportError:
-    tracemallocOK = False
-
-# The comparison of Quantities in test_memory_usage
-# fail with older versions of numpy
-from distutils.version import LooseVersion
-
-NPY_VERSION_CHECK = LooseVersion(np.version.version) >= "1.13"
-
-from .test_moments import moment_cube
 from .helpers import assert_allclose
-from ..spectral_cube import SpectralCube
 from . import utilities
 
-from astropy import convolution, units as u
+from astropy import convolution
 
 import itertools
+
+try:
+    import dask
+    DASK_OK = True
+except ImportError:
+    DASK_OK = False
 
 @pytest.mark.parametrize('use_dask, use_memmap, num_cores, parallel, verbose'.split(', '),
                          itertools.product([True,False], repeat=5))
 def test_parallel_smoothing_spatial(use_dask, use_memmap, num_cores, parallel, verbose):
+
+    if use_dask and not DASK_OK:
+        pytest.skip("Dask not installed")
 
     # BEWARE: if shape[0] == nprocs, you can get spurious passes
     cube,_ = utilities.generate_gaussian_cube(shape=(6,30,32))
@@ -53,12 +46,15 @@ def test_parallel_smoothing_spatial(use_dask, use_memmap, num_cores, parallel, v
                                        num_cores=1, parallel=False)
 
 
-    np.testing.assert_array_almost_equal(basic_result.unitless_filled_data[:],
-                                         result.unitless_filled_data[:])
+    assert_allclose(basic_result.unitless_filled_data[:],
+                    result.unitless_filled_data[:])
 
 @pytest.mark.parametrize('use_dask, use_memmap, num_cores, parallel, verbose'.split(', '),
                          itertools.product([True,False], repeat=5))
 def test_parallel_smoothing_spectral(use_dask, use_memmap, num_cores, parallel, verbose):
+
+    if use_dask and not DASK_OK:
+        pytest.skip("Dask not installed")
 
     # use asymmetric dimensions to help debugging
     cube,_ = utilities.generate_gaussian_cube(shape=(32,4,6))
@@ -80,13 +76,14 @@ def test_parallel_smoothing_spectral(use_dask, use_memmap, num_cores, parallel, 
                                         num_cores=1, parallel=False)
 
 
-    np.testing.assert_array_almost_equal(basic_result.unitless_filled_data[:],
-                                         result.unitless_filled_data[:])
+    assert_allclose(basic_result.unitless_filled_data[:],
+                    result.unitless_filled_data[:])
 
 
 
 # parametrize over the length of the spectra dimension to make sure the mapping
 # doesn't go right for the wrong reason
+@pytest.mark.skipif('not DASK_OK')
 @pytest.mark.parametrize('nspec', (4, 10, 14))
 def test_dask_apply_to_images(nspec):
     cube,_ = utilities.generate_gaussian_cube(shape=(nspec,32,32))
@@ -102,12 +99,12 @@ def test_dask_apply_to_images(nspec):
                                                         reduce=False,
                                                         use_memmap=False)
 
-    np.testing.assert_array_almost_equal(basic_result.unitless_filled_data[:],
-                                         result_nomemmap.unitless_filled_data[:])
+    assert_allclose(basic_result.unitless_filled_data[:],
+                    result_nomemmap.unitless_filled_data[:])
 
     result = cube.dask_apply_function_by_image(function=convolution.convolve,
                                                kernel=convolution.Gaussian2DKernel(2.0),
                                                projection=False, reduce=False)
 
-    np.testing.assert_array_almost_equal(basic_result.unitless_filled_data[:],
-                                         result.unitless_filled_data[:])
+    assert_allclose(basic_result.unitless_filled_data[:],
+                    result.unitless_filled_data[:])
