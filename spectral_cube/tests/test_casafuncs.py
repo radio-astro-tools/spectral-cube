dc7593b09ec16aa88398dd35627253d919bef2f1
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-13 17:41:07 +0000

    Fix CASA tests

--- spectral_cube/tests/test_casafuncs.py
+++ spectral_cube/tests/test_casafuncs.py
@@ -27,6 +27,9 @@ except ImportError:
 
 def make_casa_testimage(infile, outname):
 
+    infile = str(infile)
+    outname = str(outname)
+
     if not casaOK:
         raise Exception("Attempted to make a CASA test image in a non-CASA "
                         "environment")
@@ -54,38 +57,41 @@ def make_casa_testimage(infile, outname):
         ia.close()
 
 
+@pytest.fixture
+def filename(request):
+    return request.getfixturevalue(request.param)
+
+
 @pytest.mark.skipif(not casaOK, reason='CASA tests must be run in a CASA environment.')
-@pytest.mark.parametrize('basefn', ('adv', 'advs', 'sdav', 'vad', 'vsad'))
-def test_casa_read(basefn):
+@pytest.mark.parametrize('filename', ('data_adv', 'data_advs', 'data_sdav',
+                                      'data_vad', 'data_vsad'),
+                         indirect=['filename'])
+def test_casa_read(filename, tmp_path):
 
-    cube = SpectralCube.read(path('{0}.fits').format(basefn))
+    cube = SpectralCube.read(filename)
 
-    make_casa_testimage(path('{0}.fits').format(basefn), path('casa_{0}.image').format(basefn))
+    make_casa_testimage(filename, tmp_path / 'casa.image')
 
-    casacube = SpectralCube.read(path('casa_{0}.image').format(basefn), format='casa_image')
+    casacube = SpectralCube.read(tmp_path / 'casa.image', format='casa_image')
 
     assert casacube.shape == cube.shape
     # what other equalities should we check?
 
-    os.system('rm -rf {0}'.format(path('casa_{0}.image'.format(basefn))))
 
 @pytest.mark.skipif(not casaOK, reason='CASA tests must be run in a CASA environment.')
-def test_casa_mask():
+def test_casa_mask(data_adv, tmp_path):
 
-    cube = SpectralCube.read(path('adv.fits'))
+    cube = SpectralCube.read(data_adv)
 
     mask_array = np.array([[True, False], [False, False], [True, True]])
     bool_mask = BooleanArrayMask(mask=mask_array, wcs=cube._wcs,
                                  shape=cube.shape)
     cube = cube.with_mask(bool_mask)
 
-    if os.path.exists('casa.mask'):
-        os.system('rm -rf casa.mask')
-
-    make_casa_mask(cube, 'casa.mask', add_stokes=False,
+    make_casa_mask(cube, str(tmp_path / 'casa.mask'), add_stokes=False,
                    append_to_image=False, overwrite=True)
 
-    ia.open('casa.mask')
+    ia.open(str(tmp_path / 'casa.mask'))
 
     casa_mask = ia.getchunk()
 
@@ -117,67 +123,54 @@ def test_casa_mask():
 
 
 @pytest.mark.skipif(not casaOK, reason='CASA tests must be run in a CASA environment.')
-def test_casa_mask_append():
+def test_casa_mask_append(data_adv, tmp_path):
 
-    cube = SpectralCube.read(path('adv.fits'))
+    cube = SpectralCube.read(data_adv)
 
     mask_array = np.array([[True, False], [False, False], [True, True]])
     bool_mask = BooleanArrayMask(mask=mask_array, wcs=cube._wcs,
                                  shape=cube.shape)
     cube = cube.with_mask(bool_mask)
 
-    make_casa_testimage(path('adv.fits'), path('casa.image'))
-
-    if os.path.exists(path('casa.mask')):
-        os.system('rm -rf {0}'.format(path('casa.mask')))
-
-    maskpath = os.path.join(path('casa.image'),'casa.mask')
-    if os.path.exists(maskpath):
-        os.system('rm -rf {0}'.format(maskpath))
+    make_casa_testimage(data_adv, tmp_path / 'casa.image')
 
     # in this case, casa.mask is the name of the mask, not its path
-    make_casa_mask(cube, path('casa.mask'), append_to_image=True,
-                   img=path('casa.image'), add_stokes=False,
-                   overwrite=True)
+    make_casa_mask(cube, 'casa.mask', append_to_image=True,
+                   img=str(tmp_path / 'casa.image'), add_stokes=False, overwrite=True)
 
-    assert os.path.exists(path('casa.image/casa.mask'))
-
-    os.system('rm -rf {0}'.format(path('casa.image')))
-    os.system('rm -rf {0}'.format(path('casa.mask')))
+    assert os.path.exists(tmp_path / 'casa.image/casa.mask')
 
 
 @pytest.mark.skipif(not casaOK, reason='CASA tests must be run in a CASA environment.')
-def test_casa_beams():
+def test_casa_beams(data_adv, data_adv_beams, tmp_path):
     """
     test both ``make_casa_testimage`` and the beam reading tools using casa's
     image reader
     """
 
-    make_casa_testimage(path('adv.fits'), path('casa_adv.image'))
-    make_casa_testimage(path('adv_beams.fits'), path('casa_adv_beams.image'))
+    make_casa_testimage(data_adv, tmp_path / 'casa_adv.image')
+    make_casa_testimage(data_adv_beams, tmp_path / 'casa_adv_beams.image')
 
-    cube = SpectralCube.read(path('casa_adv.image'), format='casa_image')
+    cube = SpectralCube.read(tmp_path / 'casa_adv.image', format='casa_image')
 
     assert hasattr(cube, 'beam')
 
-    cube_beams = SpectralCube.read(path('casa_adv_beams.image'), format='casa_image')
+    cube_beams = SpectralCube.read(tmp_path / 'casa_adv_beams.image', format='casa_image')
 
     assert hasattr(cube_beams, 'beams')
     assert isinstance(cube_beams, VaryingResolutionSpectralCube)
 
-    os.system('rm -rf {0}'.format(path('casa_adv_beams.image')))
-
 
 @pytest.mark.skipif(not casaOK, reason='CASA tests must be run in a CASA environment.')
-def test_casa_arrayslicer():
+def test_casa_arrayslicer(data_adv, tmp_path):
     """
     """
 
-    make_casa_testimage(path('adv.fits'), path('casa_adv.image'))
+    make_casa_testimage(data_adv, tmp_path / 'casa_adv.image')
 
     from spectral_cube.io.casa_image import ArraylikeCasaData
 
-    arr = ArraylikeCasaData(path('casa_adv.image'))
+    arr = ArraylikeCasaData(str(tmp_path / 'casa_adv.image'))
 
     assert arr.shape == (4,3,2)
     assert arr.ndim == 3
@@ -190,7 +183,7 @@ def test_casa_arrayslicer():
     assert arr[:3,:2,0].shape == (3,2,)
     assert arr[:3,:2,1].shape == (3,2,)
 
-    cube = SpectralCube.read(path('casa_adv.image'), format='casa_image')
+    cube = SpectralCube.read(tmp_path / 'casa_adv.image', format='casa_image')
 
     assert cube[:3,:2,:1].shape == (3,2,1)
     assert np.array(cube.filled_data[:3,:2,:1]).shape == (3,2,1)
