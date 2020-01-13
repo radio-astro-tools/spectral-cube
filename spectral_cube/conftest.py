83c8754f925281ea1eadd4839d9a27c67ab20d58
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-13 17:45:59 +0000

    Simplify pytest astropy header configuration

--- spectral_cube/conftest.py
+++ spectral_cube/conftest.py
@@ -12,43 +12,22 @@ from astropy.io import fits
 from astropy import wcs
 from astropy.version import version as astropy_version
 
-if LooseVersion(astropy_version) < LooseVersion('2.0.3'):
-    # Astropy is not compatible with the standalone plugins prior this while
-    # astroquery requires them, so we need this workaround. This will mess
-    # up the test header, but everything else will work.
-    from astropy.tests.pytest_plugins import (PYTEST_HEADER_MODULES,
-                                              TESTED_VERSIONS)
-elif astropy_version < '3.0':
-    # With older versions of Astropy, we actually need to import the pytest
-    # plugins themselves in order to make them discoverable by pytest.
+from astropy.version import version as astropy_version
+
+if astropy_version < '3.0':
     from astropy.tests.pytest_plugins import *
+    del pytest_report_header
 else:
-    # As of Astropy 3.0, the pytest plugins provided by Astropy are
-    # automatically made available when Astropy is installed. This means it's
-    # not necessary to import them here, but we still need to import global
-    # variables that are used for configuration.
-    from astropy.tests.plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
-
-from astropy.tests.helper import enable_deprecations_as_exceptions
-
-## Uncomment the following line to treat all DeprecationWarnings as
-## exceptions
-# enable_deprecations_as_exceptions()
-
-# Uncomment and customize the following lines to add/remove entries
-# from the list of packages for which version numbers are displayed
-# when running the tests
-PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-PYTEST_HEADER_MODULES['regions'] = 'regions'
-PYTEST_HEADER_MODULES['APLpy'] = 'aplpy'
-
-# This is to figure out the spectral-cube's version, rather than astropy's
-#from .version import version, astropy_helpers_version
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 
 
-#packagename = os.path.basename(os.path.dirname(__file__))
-#TESTED_VERSIONS[packagename] = version
-#TESTED_VERSIONS['astropy_helpers'] = astropy_helpers_version
+def pytest_configure(config):
+
+    config.option.astropy_header = True
+
+    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+    PYTEST_HEADER_MODULES['regions'] = 'regions'
+    PYTEST_HEADER_MODULES['APLpy'] = 'aplpy'
 
 
 # Import casatools and casatasks here if available as they can otherwise
