37412178709738426a13991ea483b214e97fe0cf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-06-24 15:01:27 -0400

    more shape issues

--- spectral_cube/dask_spectral_cube.py
+++ spectral_cube/dask_spectral_cube.py
@@ -1278,8 +1278,8 @@ class DaskSpectralCubeMixin:
             else:
                 #return np.interp(args[0], args[1], y[:, 0, 0],
                 #                 left=fill_value, right=fill_value).reshape(chunkshape)
-                interp = scipy.interpolate.interp1d(args[1], y, fill_value=fill_value)
-                return interp(args[0])
+                interp = scipy.interpolate.interp1d(args[1], y.T, fill_value=fill_value)
+                return interp(args[0]).T
 
         newcube = cubedata.map_blocks(interp_wrapper,
                                       args=(spectral_grid.value, inaxis.value),
