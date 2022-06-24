f8dde12fe391cdf7f3e2aeb14e1586d455e4beb8
e-koch <koch.eric.w@gmail.com>
2022-06-24 15:44:26 -0400

    Add splitting the beam table by polarization to match how the data cube is split based on its WCS

--- spectral_cube/io/fits.py
+++ spectral_cube/io/fits.py
@@ -230,15 +230,13 @@ def load_fits_cube(input, hdu=0, meta=None, target_cls=None, use_dask=False, **k
 
     elif wcs.wcs.naxis == 4:
 
-        data, wcs = cube_utils._split_stokes(data, wcs)
-
-        # Slice our beam table by the number of velocity channels.
-        # NOTE: we don't currently have a check for the total number of beams matching
-        # nchan * npol. This just assumes that (likely fine in most cases).
-        nchan = data['I'].shape[0]
+        if beam_table is None:
+            data, wcs = cube_utils._split_stokes(data, wcs)
+        else:
+            data, wcs, beam_tables = cube_utils._split_stokes(data, wcs, beam_table=beam_table)
 
         stokes_data = {}
-        for ii, component in enumerate(data):
+        for component in data:
             comp_data, comp_wcs = cube_utils._orient(data[component], wcs)
             comp_mask = LazyMask(np.isfinite, data=comp_data, wcs=comp_wcs)
             if beam_table is None:
@@ -249,7 +247,7 @@ def load_fits_cube(input, hdu=0, meta=None, target_cls=None, use_dask=False, **k
                 stokes_data[component] = VRSC(comp_data, wcs=comp_wcs,
                                               mask=comp_mask, meta=meta,
                                               header=header,
-                                              beam_table=beam_table[nchan*ii:nchan*(ii+1)],
+                                              beam_table=beam_tables[component],
                                               major_unit=beam_units[0],
                                               minor_unit=beam_units[1]
                                              )
