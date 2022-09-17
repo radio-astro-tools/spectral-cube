e427654aac874295940799b6b35443f66622fb4e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-09-17 13:26:23 -0400

    check that loop_in_thread can be imported

--- spectral_cube/tests/test_dask.py
+++ spectral_cube/tests/test_dask.py
@@ -12,7 +12,7 @@ from astropy import units as u
 from astropy.utils import data
 
 try:
-    from distributed.utils_test import client, loop, cluster_fixture, cleanup  # noqa
+    from distributed.utils_test import client, loop, cluster_fixture, cleanup, loop_in_thread  # noqa
     DISTRIBUTED_INSTALLED = True
 except ImportError:
     DISTRIBUTED_INSTALLED = False
