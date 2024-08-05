ce6b9e69b8c8b62b7e0279205429512bb0d2db15
Eric Koch <koch.eric.w@gmail.com>
2024-08-05 17:31:51 -0400

    only pass str to casa tasks

--- spectral_cube/conftest.py
+++ spectral_cube/conftest.py
@@ -396,8 +396,8 @@ def data_vda_beams_image(tmp_path):
     hdul.writeto(tmp_path / 'vda_beams.fits')
     from casatools import image
     ia = image()
-    ia.fromfits(infile=tmp_path / 'vda_beams.fits',
-                outfile=tmp_path / 'vda_beams.image',
+    ia.fromfits(infile=str(tmp_path / 'vda_beams.fits'),
+                outfile=str(tmp_path / 'vda_beams.image'),
                 overwrite=True)
     for (bmaj, bmin, bpa, chan, pol) in beams.data:
         ia.setrestoringbeam(major={'unit': 'arcsec', 'value': bmaj},
